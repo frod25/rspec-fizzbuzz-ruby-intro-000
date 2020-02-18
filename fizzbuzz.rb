71dd2e3c76d86d70c6d64908caa6ee5d06e2e718
Felix Rodriguez <frod25@gmail.com>
2020-02-18 18:57:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if int % 3 && int % 5 == 0
-    "FizzBuzz"
-  elsif int % 3 == 0
+  if int % 3 == 0
     "Fizz"
-  else int % 5 == 0
+  elsif int % 5 == 0
     "Buzz"
+  else int % 5 == 0 && int % 3 == 0
+    "FizzBuzz"
   end
 end
